a5aeab1b429c79a9cdf42a274c0fb3a43ecb607d
Jason Studstill <standstill64@gmail.com>
2015-07-15 21:32:36 -0700

    Implement 2 tests: 1) Server responds with statusCode 200; 2) Root url request '/' responds with and object (message)

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
+var startServer = require('./server.js').startServer;
 
 gulp.task('default', ['lint'], function() {});
+
+// gulp.task('serve', function() {
+//   startServer();
+// });
+
+gulp.task('test', function() {
+  return gulp.src(['*.js', '*/*.js'])
+             .pipe(mocha());
+});
 
 gulp.task('lint', function() {
   return gulp.src(['*.js', '*/*.js'])
              .pipe(jshint())
              .pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.watch(['*.js', 'test/*js'], ['test', 'lint'], function() {});
 
 
 
