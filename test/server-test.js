dcdd3597276f71144d94a064e9dcbc65e5e5fca3
Jason Studstill <standstill64@gmail.com>
2015-07-15 23:25:20 -0700

    Test for POST request WIP

--- test/server-test.js
+++ test/server-test.js
@@ -1,6 +1,7 @@
 var chai = require('chai');
 var expect = require('chai').expect;
 var chaiHttp = require('chai-http');
+var fs = require('fs');
 var startServer = require('../server').startServer;
 
 chai.use(chaiHttp);
@@ -14,7 +15,6 @@ describe('server.js', function() {
     chai.request('localhost:3000')
       .get('/')
       .then(function(response) {
-        console.log(response.statusCode);
         expect(response.statusCode).to.eql(200);
         done();
       });
@@ -24,8 +24,46 @@ describe('server.js', function() {
     chai.request('localhost:3000')
       .get('/')
       .then(function(response) {
-        console.log(response.body);
         expect(typeof(response.body)).to.equal('object');
+        done();
+      });
+  });
+
+  it('should return the contents of the data folder on a ' +
+      'GET request to "/data"', function(done) {
+      chai.request('localhost:3000')
+        .get('/data')
+        .then(function(response) {
+          expect(fs.readdirSync('./data')).to.eql(response.body);
+          done();
+        });
+  });
+
+  it('should return statusCode 404 if invalid route', function(done) {
+    chai.request('localhost:3000')
+      .get('/lfdkjoie98097')
+      .then(function(response) {
+        expect(response.statusCode).to.eql(404);
+        done();
+      });
+  });
+
+  it('should save a new file on "POST request ONLY if path is "/data"', function(done) {
+    chai.request('localhost:3000')
+      .post('/somethingElse')
+      .send({ name: 'Jason'})
+      .then(function(response) {
+        expect(response.statusCode).to.eql(404);
+        done();
+      });
+  });
+
+  it('should save a new file on "POST request', function(done) {
+    chai.request('localhost:3000')
+      .post('/data')
+      .send({ name: 'Jason'})
+      .then(function(response) {
+        // expect a new file to be created
         done();
       });
   });
