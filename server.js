a5b49b425e99d4014493dbf80e5ec8f9fbc65abd
Jason Studstill <standstill64@gmail.com>
2015-07-15 23:23:33 -0700

    Server creates new file on POST but contents are undefined

--- server.js
+++ server.js
@@ -1,20 +1,56 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 var port = 3000;
 
-function startServer(route, handle) {
+function startServer() {
 
-  function onRequest(request, response) {
-    var pathName = url.parse(request.url).pathname;
-    console.log('Request for ' + pathName + ' received');
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.write('{"message": "You Made It!"}');
+  function notFound404(request, response) {
+    response.writeHead(404, {'Content-Type': 'application/json'});
+    response.write('{"message":"Invalid Route"}');
     response.end();
   }
 
+  function onRequest(request, response) {
+    if (request.method === 'GET') {
+      if (request.url === '/' || request.url === '/index') {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.write('{"message": "You are at the root URL"}');
+        response.end();
+      } else if (request.url === '/data') {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        fs.readdir('./data', function(err, files) {
+          response.write(JSON.stringify(files));
+          response.end();
+        });
+      } else {
+        notFound404(request, response);
+      }
+    } else if (request.method === 'POST') {
+      if (request.url !== '/data') {
+        notFound404(request, response);
+      } else {
+
+        console.log(JSON.stringify(response.body));
+        response.on('data', function(chunk) {
+          console.log('BODY: ' + chunk);
+        });
+        // Find which files are already in the directory
+        fs.readdir('./data', function(err, files) {
+          var fileNum = files.length + 1;
+          // Build up new file name and write POST request data
+          fs.writeFile('data/file' + fileNum + '.json', request.body, function(err) {
+            if (err) throw err;
+            console.log('It saved!');
+          });
+        });
+      }
+    }
+  }
   http.createServer(onRequest).listen(port);
   console.log('Server Started on Port 3000');
 }
+
 
 exports.startServer = startServer;
 
