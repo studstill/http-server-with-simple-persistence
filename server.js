a66498f8b022af27b0e49856ab8152921d87f229
Jason Studstill <standstill64@gmail.com>
2015-07-15 21:33:35 -0700

    Server starts and sends message object {"message": "You Made It!"}

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var http = require('http');
+var url = require('url');
+var port = 3000;
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, '{ application/json');
-  response.write('SOMETHING HAPPENED');
-});
+function startServer(route, handle) {
 
-server.listen(3000, function() {
-  console.log('server started');
-});
+  function onRequest(request, response) {
+    var pathName = url.parse(request.url).pathname;
+    console.log('Request for ' + pathName + ' received');
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.write('{"message": "You Made It!"}');
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(port);
+  console.log('Server Started on Port 3000');
+}
+
+exports.startServer = startServer;
+
+
